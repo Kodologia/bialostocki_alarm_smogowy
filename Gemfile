b91a75e066fd0ef6826a8fab0d9252db7ea0ae8b
Lukasz Pelszynski <lpelszyn@o2.pl>
2017-10-31 22:08:29 +0100

    Fix problems

--- Gemfile
+++ Gemfile
@@ -1,8 +1,6 @@
 source 'https://rubygems.org' do
-	
 	gem 'pry'
 	gem 'nokogiri'
 	gem 'sinatra'
-	gem 'colorize'
-
+	gem 'sinatra-contrib'
 end
--- smog.rb
+++ smog.rb
@@ -3,35 +3,26 @@ require 'rubygems'
 require 'pry'
 require 'nokogiri'
 require 'sinatra'
-require 'colorize'
+require "sinatra/reloader" if development?
 
-uri = URI('http://www.wios.bialystok.pl/?go=airmiegod')
-uploaded_page_data = Net::HTTP.get(uri) # => String
-page_text= Nokogiri::HTML(uploaded_page_data)
-
-
-
-value = (page_text.css("//.centr//td").last).to_s
+def pobierz_kolor_z_netu
+	uri = URI('http://www.wios.bialystok.pl/?go=airmiegod')
+	uploaded_page_data = Net::HTTP.get(uri) # => String
+	page_text= Nokogiri::HTML(uploaded_page_data)
+	page_text.css("//.centr//td").last.text
+end
 
 def style_color(x)
 	x = x.to_i
 	if x > 10
-		'blue'
+		'red'
 	else
 		'green'
 	end
 end
 
-color = style_color(value)
-
-
-
-
 get '/' do
-	@value = value
-	@color = color
+	@value = pobierz_kolor_z_netu()
+	@color = style_color(@value)
 	erb :index
 end
-
-binding.pry
-
