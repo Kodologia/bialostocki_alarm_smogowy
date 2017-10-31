02fa9c0f92476b3b8cd6c6ecaa3c15b20377c7dd
zsrr1991 <zsrr1991@gmail.com>
2017-10-31 11:33:36 +0100

    Added warning message colour

--- Gemfile
+++ Gemfile
@@ -3,5 +3,6 @@ source 'https://rubygems.org' do
 	gem 'pry'
 	gem 'nokogiri'
 	gem 'sinatra'
+	gem 'colorize'
 
 end
--- smog.rb
+++ smog.rb
@@ -3,6 +3,7 @@ require 'rubygems'
 require 'pry'
 require 'nokogiri'
 require 'sinatra'
+require 'colorize'
 
 uri = URI('http://www.wios.bialystok.pl/?go=airmiegod')
 uploaded_page_data = Net::HTTP.get(uri) # => String
@@ -10,11 +11,22 @@ page_text= Nokogiri::HTML(uploaded_page_data)
 
 
 
-puts page_text.css("//.centr//td").last
+value = page_text.css("//.centr//td").last
+
+def style_color(x)
+	if x.to_s > "1"
+		'red'
+	else
+		'black'
+	end
+end
+
+color = style_color(value)
 
 get '/' do
-	"Stezenie czasteczek wynosi"
-	
+	@value = value
+	@color = color
+	erb :index
 end
 
 
